d470f72c753e9666212bbb2d984ab3446b845487
Meguipascual <meguipascual@gmail.com>
2022-01-20 17:36:20 +0100

    Now the forward/backward movement is made by a force instead of a transform modification.

--- LessonOne/Assets/Scripts/PlayerController.cs
+++ LessonOne/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 20f;
+    [SerializeField] private float horsePower = 20f;
     [SerializeField] private float turnSpeed = 45f;
     private float horizontalInput;
     private float verticalInput;
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,7 +22,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         //Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
