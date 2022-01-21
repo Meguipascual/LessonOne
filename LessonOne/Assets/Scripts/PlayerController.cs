3e2f71d69a10ec1cbdbacb01dce8af7d2653e801
Meguipascual <meguipascual@gmail.com>
2022-01-21 19:55:20 +0100

    This time I've created an Speedometer with a text mesh pro GUI

--- LessonOne/Assets/Scripts/PlayerController.cs
+++ LessonOne/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horsePower = 20f;
     [SerializeField] private float turnSpeed = 45f;
+    [SerializeField] float speed;
     [SerializeField] private GameObject centerOfMass;
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody playerRb;
+    [SerializeField] TextMeshProUGUI speedometerText; 
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +27,15 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         //Moves the car forward based on vertical input
-        playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+        
+        if (speed < 180)
+        {
+            playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+        }
+        
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f); //2.237 for Mph
+        speedometerText.SetText("Speed: " + speed + " Km/h" );
     }
 }
