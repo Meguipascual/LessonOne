4a93685b2b91141af9c555e1aeeee0e000a87426
Meguipascual <meguipascual@gmail.com>
2022-01-27 20:34:00 +0100

    RPM system added also now we can prevent the car to accelerate when in air.

--- LessonOne/Assets/Scripts/PlayerController.cs
+++ LessonOne/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horsePower = 20f;
     [SerializeField] private float turnSpeed = 45f;
     [SerializeField] float speed;
+    [SerializeField] float rpm;
     [SerializeField] private GameObject centerOfMass;
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody playerRb;
-    [SerializeField] TextMeshProUGUI speedometerText; 
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,20 +27,48 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         //Moves the car forward based on vertical input
-        
-        if (speed < 180)
+        if (IsCarOnGround())
         {
-            playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+
+            if (speed < 180)
+            {
+                playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+            }
+
+            //Rotates the car based on horizontal input
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f); //2.237 for Mph
+            speedometerText.SetText("Speed: " + speed + " Km/h");
+
+            rpm = (speed % 25) * 100;
+            rpmText.SetText("RPM: " + rpm);
         }
-        
-        //Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f); //2.237 for Mph
-        speedometerText.SetText("Speed: " + speed + " Km/h" );
+    }
+    bool IsCarOnGround() 
+    {
+        wheelsOnGround = 0;
+
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded) 
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
